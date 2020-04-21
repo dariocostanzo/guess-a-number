d17300eda906eef081c6546330552630ca478426
dariocostanzo <darioc@xara.com>
2020-04-21 18:18:22 +0100

    Adds components

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import Header from './components/header';
+
+import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = selectedNumber => {
+    setUserNumber(selectedNumber);
+  };
+  // Passing down a reference to startGameHandler to that component on the onStartGameProp
+  // we forward the startGameHandler a pointer at setGameHandler
+  // and we can use onStartGame (our new property) in StartGameScreen.js in "STAR GAME BUTTON"
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  //if a number was chosen
+  if (userNumber) {
+    content = <GameScreen userChoice={userNumber} />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title='Guess a Number' />
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,120 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
-import Card from '../components/Card';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert
+} from 'react-native';
 
-const StartGameScreen = (props) => {
-  return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>Start a new Game!</Text>
-      <Card style={styles.inputContainer}>
-        <Text>Select a Number</Text>
-        <TextInput />
-        <View style={styles.ButtonContainer}>
-          <Button title='Reset' onPress={() => {}} />
-          <Button title='Confirm' onPress={() => {}} />
-        </View>
+import Card from '../components/Card';
+import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
+import Colors from '../constants/colors';
+
+const StartGameScreen = props => {
+  //Set up the usestate call and register some state
+  const [enteredValue, setEneteredValue] = useState('');
+  // numberInputHandler will get the inputText and that's the function which I want to connect to my input
+  // and there to "onChangeText", point at the numberInputHandler and feed the value back in and that's our entered
+  // value.
+  // onChangeText={numberInputHandler}
+  // value={enteredValue}
+
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const numberInputHandler = inputText => {
+    // To validate the input I set the entereValue to inputText where I replace some content based on a regular expression
+    // /[replacing anything that's not a number]/ g = globally, with an '' empty string
+    setEneteredValue(inputText.replace(/[^0-9]/g, ''));
+  };
+
+  // resetInputHandler calls setEnteredValue which is our state setting function to update the state we feed back
+  // into the input and set this to an empty string
+  const resetInputHandler = () => {
+    setEneteredValue('');
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert(
+        'Invalid number!',
+        'Number has to be a number between 1 and 99',
+        [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }]
+      );
+      return;
+    }
+    setConfirmed(true);
+    // set the endered value with parseInt by passing the entered value, so I set this to be my selected number
+    setSelectedNumber(chosenNumber);
+    setEneteredValue(''); //will be queued by React and prcessed the next time the component is rendered
+    Keyboard.dismiss();
+  };
+
+  let confirmedOutupt;
+
+  if (confirmed) {
+    confirmedOutupt = (
+      <Card style={styles.summaryContainer}>
+        <Text>You selectd</Text>
+        <NumberContainer>{selectedNumber}</NumberContainer>
+        <Button
+          title='START GAME'
+          // onStartGame is a property of <StartGameScreen> in App.js and forward the currently selected number
+          // to onStartGame, managed in the state of StartGameScreen bound to the startGameHandler in App.js
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
       </Card>
-    </View>
+    );
+  }
+
+  return (
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>Start a new Game!</Text>
+        <Card style={styles.inputContainer}>
+          <Text>Select a Number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            autoCapitalize='none'
+            autoCorrect={false}
+            keyboardType='number-pad'
+            maxLength={2}
+            onChangeText={numberInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.ButtonContainer}>
+            <View style={styles.button}>
+              <Button
+                style={styles.button}
+                color={Colors.accent}
+                title='Reset'
+                onPress={resetInputHandler}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                style={styles.button}
+                color={Colors.primary}
+                title='Confirm'
+                onPress={confirmInputHandler}
+              />
+            </View>
+          </View>
+        </Card>
+        {confirmedOutupt}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -39,6 +139,17 @@ const styles = StyleSheet.create({
     //the buttons sit on the left and right edge of the row and distribute all the available free space between them
     justifyContent: 'space-between',
     paddingHorizontal: 15 // adds some spacing to the left and right
+  },
+  button: {
+    width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
+  },
+  summaryContainer: {
+    marginTop: 20,
+    alignItems: 'center'
   }
 });
 
